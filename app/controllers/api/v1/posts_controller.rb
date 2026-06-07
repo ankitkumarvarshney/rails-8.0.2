c6dabe77c0ee8273d974b127a720f358d6f87dca
Ankit Varshney <imankitvarshney@gmail.com>
2026-06-07 16:10:38 +0000

    user specific authorization added

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,28 +1,48 @@
 module Api
   module V1
     class PostsController < Api::BaseController
+      before_action :authorize_owner, only: [:update, :destroy]
       def index
-        render json: Post.order(updated_at: :desc)
+        render json: Post.includes(:user).order(updated_at: :desc).as_json(
+          include: { user: { only: [:id, :email] } },
+          except: [:created_at, :updated_at]
+        )
       end
 
       def create
-        post = Post.create!(post_params)
-        render json: post, status: :created
+        post = @current_user.posts.create!(post_params)
+        render json: post.as_json(
+          include: { user: { only: [:id, :email] } },
+          except: [:created_at, :updated_at]
+        ), status: :created
       end
 
       def show
-        render json: Post.find(params[:id])
+        render json: Post.includes(:user).find(params[:id]).as_json(
+          include: { user: { only: [:id, :email] } },
+          except: [:created_at, :updated_at]
+        )
       end
 
       def update
         post = Post.find(params[:id])
         post.update!(post_params)
-        render json: post
+        render json: post.as_json(
+          include: { user: { only: [:id, :email] } },
+          except: [:created_at, :updated_at]
+        )
       end
 
       def destroy
         Post.find(params[:id]).destroy
         head :no_content
+      end
+
+      def authorize_owner
+        post = Post.find(params[:id])
+        unless post.user == @current_user
+          render json: { error: "Forbidden" }, status: :forbidden
+        end
       end
 
     private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
+  belongs_to :user, optional: true
   validates :title, :content, presence: true
 end
